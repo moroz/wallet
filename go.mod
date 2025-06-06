a1e115a9fb8278a52db1c1070b8a48cbddf44453
Karol Moroz <karol@moroz.dev>
2025-06-06 23:01:42 +0800

    Store seed an mnemonic in db

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/moroz/wallet
 go 1.24.3
 
 require (
+	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
+	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/term v0.32.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"log"
+	"os/exec"
 	"syscall"
 
+	"github.com/tyler-smith/go-bip39"
 	"golang.org/x/term"
 )
 
@@ -34,10 +39,62 @@ func getPassphrase() (string, error) {
 	return pw1, nil
 }
 
+func createDB(bin, dbPath, passphrase string) error {
+	stdin := bytes.NewBufferString(fmt.Sprintf("%s\n%s\n", passphrase, passphrase))
+
+	var stdout bytes.Buffer
+	cmd := exec.Command(bin, "db-create", dbPath, "-p")
+	cmd.Stdin = stdin
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stdout
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
+
+func addEntryToDB(bin, dbPath, passphrase, entryName, password string) error {
+	stdin := bytes.NewBufferString(fmt.Sprintf("%s\n%s\n", passphrase, password))
+
+	var stdout bytes.Buffer
+	cmd := exec.Command(bin, "add", dbPath, "-p", entryName)
+	cmd.Stdin = stdin
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stdout
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
+
 func main() {
+	bin, err := exec.LookPath("keepassxc-cli")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	passphrase, err := getPassphrase()
 	if err != nil {
 		log.Fatalf("Failed to create a passphrase for database: %s", err)
 	}
-	fmt.Println(string(passphrase))
+
+	err = createDB(bin, "./wallet.kdbx", passphrase)
+
+	var seed = make([]byte, 32)
+	_, err = rand.Read(seed)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mnemonic, _ := bip39.NewMnemonic(seed)
+
+	err = addEntryToDB(bin, "./wallet.kdbx", passphrase, "WALLET_SEED", base64.StdEncoding.EncodeToString(seed))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = addEntryToDB(bin, "./wallet.kdbx", passphrase, "WALLET_MNEMONIC", mnemonic)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
